26c722266ef181f368d9844f937666c46ce2659d
Luis Saurith Alvarado <sauriths@gmail.com>
2022-08-29 15:53:51 -0500

    Commint Inicial

---
+++ src/main/java/com/myacademico/MyacademicoApplication.java
@@ -0,0 +1,13 @@
+package com.myacademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyacademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyacademicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myacademico/MyacademicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myacademico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyacademicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
